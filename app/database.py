e8bdfc4ba2529d48a882ed1d20794871e308c81b
trevorcourville <105018752+trevorcourville@users.noreply.github.com>
2026-03-02 18:12:13 -0600

    Database session dependency

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, session
+from fastapi import Depends
 
 DATABASE_URL = "sqlite:///./app.db"
 
@@ -8,3 +9,11 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
